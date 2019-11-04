32cf5debbee84bad076c5eee8f469041facafd4d
Alfred Lemon <greymagez@hotmail.com>
2019-11-04 12:14:49 -0500

    Add user and all organization list

--- frontend/src/views/admin/Users/Users.js
+++ frontend/src/views/admin/Users/Users.js
@@ -12,9 +12,11 @@ import DialogActions from "@material-ui/core/DialogActions";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
+import DeleteIcon from "@material-ui/icons/Delete";
 import GroupIcon from "@material-ui/icons/Group";
 import EnhancedEncryptionIcon from "@material-ui/icons/EnhancedEncryption";
 
@@ -23,35 +25,108 @@ import uniqid from "uniqid";
 const MaterialTable = lazy(() => import("material-table"));
 
 import "./Users.scss";
+import IconButton from "@material-ui/core/IconButton";
 
-const UserOrgnaizationsOrganizationsDialogContent = ({  }) => {
+const OrganizationsListItems = ({ organizations, handleAddOrganization }) => organizations.map((organization) => {
+  const { name } = organization;
 
-};
-
-const UserOrganizationsCurrentOrganizationsDialogContent = () => {
-
-};
-
-const UserOrganizationsDialogContent = ({ userOrganizations }) => {
+  const handleAddOrganizationItem = () => handleAddOrganization(organization);
 
   return (
-    <DialogContent>
-      
+    <ListItem key={uniqid()} button onClick={handleAddOrganizationItem}>
+      <ListItemText primary={name}/>
+    </ListItem>
+  )
+});
+
+const OrganizationsList = ({ organizations }) => (
+  <List>
+    <OrganizationsListItems organizations={organizations}/>
+  </List>
+);
+
+const AllOrganizationsDialogContent = ({ organizations, handleQueryChange }) => {
+  return (
+    <div className="allOrganizationsContent">
+      <TextField label="Search Organizations..." type="search" onChange={handleQueryChange} fullWidth/>
+      <OrganizationsList organizations={organizations}/>
+    </div>
+  );
+};
+
+const DeleteButton = ({ handleDelete }) => (
+  <IconButton onClick={handleDelete} aria-label="delete">
+    <DeleteIcon/>
+  </IconButton>
+);
+
+const UserOrganizationsListItemActions = ({ handleDeleteUserOrganization }) => (
+  <ListItemSecondaryAction>
+    <DeleteButton handleDelete={handleDeleteUserOrganization}/>
+  </ListItemSecondaryAction>
+);
+
+const UserOrganizationsListItems = ({ userOrganizations, handleDeleteUserOrganization }) => userOrganizations.map((userOrganization) => {
+  const { name } = userOrganization;
+
+  const handleDeleteUserOrganizationItem = () => handleDeleteUserOrganization(userOrganization);
+
+  return (
+    <ListItem key={uniqid()} button>
+      <ListItemText primary={name}/>
+      <UserOrganizationsListItemActions handleDeleteUserOrganizationItem={handleDeleteUserOrganizationItem}/>
+    </ListItem>
+  );
+});
+
+const UserOrganizationsList = ({ userOrganizations }) => (
+  <List>
+    <UserOrganizationsListItems userOrganizations={userOrganizations}/>
+  </List>
+);
+
+const UserOrganizationsDialogContent = ({ userOrganizations, handleQueryChange }) => {
+  return (
+    <div className="userOrganizationsContent">
+      <TextField label="Search User Organizations..." type="search" onChange={handleQueryChange} autoFocus fullWidth/>
+      <UserOrganizationsList userOrganizations={userOrganizations}/>
+    </div>
+  );
+};
+
+const OrganizationsDialogContent = ({ userOrganizations, organizations }) => {
+  const [ userOrganizationsQuery, setUserOrganizationsQuery ] = useState("");
+  const [ organizationsQuery, setOrganizationsQuery ] = useState("");
+
+  let filteredUserOrganizations = userOrganizations.filter(({ name }) => name.toLowerCase().includes(userOrganizationsQuery.toLowerCase()));
+  let filteredOrganizations = organizations.filter(({ name }) => name.toLowerCase().includes(organizationsQuery.toLowerCase()));
+
+  const handleOrganizationsQueryChange = ({ target: { value } }) => setOrganizationsQuery(value);
+  const handleUserOrganizationsQueryChange = ({ target: { value } }) => setUserOrganizationsQuery(value);
+
+  const handleDeleteUserOrganization = (userOrganization) => {};
+  const handleAddOrganization = (organization) => {};
+
+  return (
+    <DialogContent className="organizationsContent">
+      <UserOrganizationsDialogContent userOrganizations={filteredUserOrganizations} handleQueryChange={handleUserOrganizationsQueryChange}/>
+      <div className="verticalSeparator"></div>
+      <AllOrganizationsDialogContent organizations={filteredOrganizations} handleQueryChange={handleOrganizationsQueryChange}/>
     </DialogContent>
   );
 };
 
-const UserOrganizationsDialogActions = () => (
+const OrganizationsDialogActions = ({ handleClose }) => (
   <DialogActions>
-
+    <Button onClick={handleClose}>Close</Button>
   </DialogActions>
 );
 
-const UserOrganizationsDialog = ({ open, userOrganizations, handleClose }) => (
+const OrganizationsDialog = ({ open, userOrganizations, organizations, handleClose }) => (
   <Dialog open={open} onClose={handleClose}>
-    <DialogTitle>User Orgnaizations</DialogTitle>
-    <UserOrganizationsDialogContent userOrganizations={userOrganizations}/>
-    <UserOrganizationsDialogActions/>
+    <DialogTitle>User Organizations</DialogTitle>
+    <OrganizationsDialogContent userOrganizations={userOrganizations} organizations={organizations}/>
+    <OrganizationsDialogActions handleClose={handleClose}/>
   </Dialog>
 );
 
@@ -188,7 +263,7 @@ const Users = () => {
   return (
     <div className="usersPage">
       <MaterialTable className="usersTable" title="Users" columns={columns} actions={actions} data={users} editable={editable} options={options}/>
-      <UserOrganizationsDialog open={isOrganizationsDialogOpen} userOrganizations={userOrganizations} handleClose={handleCloseOrganizationsDialog}/>
+      <OrganizationsDialog open={isOrganizationsDialogOpen} userOrganizations={userOrganizations} organizations={organizations} handleClose={handleCloseOrganizationsDialog}/>
     </div>
   );
 };
