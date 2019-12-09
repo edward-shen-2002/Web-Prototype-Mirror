56e5ee019a6c340a2ec2d3d85297937ac5ba1911
Alfred Lemon <greymagez@hotmail.com>
2019-12-09 11:28:52 -0500

    Add header resize indicators

--- frontend/src/tools/components/Excel/Sheet/Cell/ColumnHeaderCell.js
+++ frontend/src/tools/components/Excel/Sheet/Cell/ColumnHeaderCell.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { columnNumberToName } from "xlsx-populate/lib/addressConverter";
+
+const ColumnDragger = () => {
+
+  const [ isIndicatorActive, setIsIndicatorActive ] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsIndicatorActive(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsIndicatorActive(false);
+  };
+
+  const handleMouseClick = () => {
+    // Prevent propagation?
+  };
+
+  return (
+    <div 
+      className={`columnDragger ${isIndicatorActive ? "columnDragger--indicator" : "" }`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+    </div>
+  );
+};
+
 
 const ColumnHeaderCell = ({ style, column, handleClickColumnHeader }) => {
   const handleClick = ({ ctrlKey }) => handleClickColumnHeader(column, ctrlKey);
@@ -9,7 +36,8 @@ const ColumnHeaderCell = ({ style, column, handleClickColumnHeader }) => {
 
   return (
     <div className="cell cell--positionIndicator" style={style} onClick={handleClick}>
-      {value}
+      <div>{value}</div>
+      <ColumnDragger/>
     </div>
   );
 };
--- frontend/src/tools/components/Excel/Sheet/Cell/RowHeaderCell.js
+++ frontend/src/tools/components/Excel/Sheet/Cell/RowHeaderCell.js
@@ -1,4 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+const RowDragger = () => {
+
+  const [ isIndicatorActive, setIsIndicatorActive ] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsIndicatorActive(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsIndicatorActive(false);
+  };
+
+  const handleMouseClick = () => {
+    // Prevent propagation?
+  };
+
+  return (
+    <div 
+      className={`rowDragger ${isIndicatorActive ? "rowDragger--indicator" : "" }`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {/* <hr/> */}
+    </div>
+  );
+};
 
 const RowHeaderCell = ({ style, row, handleClickRowHeader }) => {
   const handleClick = ({ ctrlKey }) => handleClickRowHeader(row, ctrlKey);
@@ -6,8 +33,9 @@ const RowHeaderCell = ({ style, row, handleClickRowHeader }) => {
   const value = row;
 
   return (
-    <div className="cell cell--positionIndicator" style={style} onClick={handleClick}>
-      {value}
+    <div className="cell cell--positionIndicator cell--rowHeader" style={style} onClick={handleClick}>
+      <div>{value}</div>
+      <RowDragger/>
     </div>
   );
 };
