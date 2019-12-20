9d8e475796d54d36cc4f20ccc23d17fddb48a450
Alfred Lemon <greymagez@hotmail.com>
2019-12-20 10:46:06 -0500

    Add simple text dialog

---
+++ frontend/src/tools/components/TextDialog/TextDialog.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContentText from "@material-ui/core/DialogContentText"
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+
+import "./TextDialog.scss";
+
+const TextDialogContent = ({ message }) => (
+  <DialogContent>
+    <DialogContentText>{message}</DialogContentText>
+  </DialogContent>
+);
+
+const TextDialogActionsButtons = ({
+  handleDelete,
+  handleClose
+}) => (
+  <ButtonGroup>
+    <Button color="secondary" variant="contained" onClick={handleDelete}>Delete</Button>
+    <Button variant="contained" onClick={handleClose}>Cancel</Button>
+  </ButtonGroup>
+);
+
+const TextDialogActions = ({ handleClose }) => (
+  <DialogActions>
+    <TextDialogActionsButtons handleClose={handleClose}/>
+  </DialogActions>
+);
+
+const TextDialog = ({
+  open,
+  title, 
+  message,
+  handleDelete,
+  handleClose
+}) => (
+  <Dialog open={open} onClose={handleClose}>
+    <DialogTitle>{title}</DialogTitle>
+    <TextDialogContent message={message}/>
+    <TextDialogActions handleDelete={handleDelete} handleClose={handleClose}/>
+  </Dialog>
+);
+
+export default TextDialog;
