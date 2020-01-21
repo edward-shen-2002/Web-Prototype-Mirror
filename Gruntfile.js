367d4d6b6fb549178bb51d639550f41c17361cc4
Alfred Lemon <greymagez@hotmail.com>
2020-01-21 10:06:12 -0500

    Temporary fix for pivotal/production - server contains frontend instead of separate

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,94 +1,14 @@
-// const pivotal = require("./config/cloud/pivotal");
-
-// console.log(pivotal)
-
-// module.exports = function(grunt) {
-//   grunt.loadNpmTasks('grunt-contrib-copy');
-//   grunt.loadNpmTasks('grunt-contrib-clean');
-//   grunt.loadNpmTasks('grunt-run');
-//   grunt.loadNpmTasks('grunt-env');
-  
-//   const backendFilesConfig = {
-//     expand: true,
-//     cwd: "./backend",
-//     src: [ 
-//       "constants/**", 
-//       "controller/**", 
-//       "models/**", 
-//       "setup/**", 
-//       "tools/**", 
-//       "app.js", 
-//       "index.js", 
-//       "package.json", 
-//       "yarn.lock" 
-//     ],
-//     dest: "build/zip"
-//   };
-
-//   const frontendFilesConfig = {
-//     expand: true,
-//     cwd: "./frontend/build",
-//     src: [ "**" ],
-//     dest: "build/zip/public/react"
-//   };
-
-//   const projectConfig = {
-//     expand: true,
-//     cwd: '..',
-//     src: [
-//       'config/**'
-//     ],
-//     dest: 'build/zip/'
-//   };
-
-//   grunt.initConfig({
-//     // Allows references to properties/scripts in package.json
-//     pkg: grunt.file.readJSON("package.json"),
-//     copy: {
-//       main: {
-//         expand: true,
-//         files: [ projectConfig, backendFilesConfig, /* awsFilesConfig, */ frontendFilesConfig ]
-//       }
-//     },
-//     clean: [ "build" ],
-//     run: {
-//       buildFrontend: {
-//         exec: "yarn --cwd frontend build:cra"
-//       },
-//       pivotal: {
-//         exec: `cd ./build/zip && cf push ${pivotal.appName} -c "node index.js"`
-//       }
-//     },
-//     env: { pivotal }
-//   });
-
-//   // mkdir for zip archive
-//   grunt.registerTask("mkdir", function () {
-//     grunt.file.mkdir('build/zip/temp');
-//     grunt.file.mkdir('build/zip/uploads');
-//     grunt.file.mkdir('build/zip/public/react');
-//   });
-
-//   // registerTask(taskName, taskList)
-//   grunt.registerTask("copy:files", ["copy:main"]);
-//   grunt.registerTask("pivotal:build", ["clean", "env:pivotal", "run:buildFrontend", "mkdir", "copy:main"]);
-//   grunt.registerTask("pivotal:publish", ["run:pivotal"]);
-//   grunt.registerTask("build:pivotal", ["pivotal:build", "pivotal:publish"]);
-// };
-
 const pivotal = require("./config/pivotal");
-const aws = require("./config/aws");
 
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-run');
-  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-run");
+  grunt.loadNpmTasks("grunt-env");
   
   const backendFilesConfig = {
     expand: true,
-    cwd: "./backend",
+    cwd: "backend",
     src: [
       "constants/**", 
       "controller/**", 
@@ -100,23 +20,23 @@ module.exports = function(grunt) {
       "package.json", 
       "yarn.lock" 
     ],
-    dest: "build/zip"
+    dest: "build"
   };
 
   const frontendFilesConfig = {
     expand: true,
-    cwd: "./frontend/build",
+    cwd: "frontend/build",
     src: [ "**" ],
-    dest: "build/zip/public"
+    dest: "build/public"
   };
 
   const projectConfig = {
     expand: true,
-    cwd: '..',
+    cwd: "..",
     src: [
-      'config/**'
+      "config/**"
     ],
-    dest: 'build/zip/'
+    dest: "build"
   };
 
   grunt.initConfig({
@@ -125,44 +45,25 @@ module.exports = function(grunt) {
     copy: {
       main: {
         expand: true,
-        files: [ projectConfig, backendFilesConfig, /* awsFilesConfig, */ frontendFilesConfig ]
+        files: [ projectConfig, backendFilesConfig, frontendFilesConfig ]
       }
     },
-    compress: {
-      main: {
-        options: {
-          archive: 'release-beta.zip'
-        },
-        expand: true,
-        cwd: 'dist/zip',
-        src: ['**/*', '.*/*'],
-        dest: './',
-      }
-    },
-    clean: ["build", "release-beta.zip"],
+    clean: [ "build" ],
     run: {
-      report: {
-        exec: "mocha ./test/main.js ./test/ --recursive --exit --check-leaks -R mochawesome"
-      },
       buildFrontend: {
         exec: "yarn run build:frontend:cra"
       },
       pivotal: {
-        exec: `cd ./build/zip && cf push ${pivotal.appName} -c "node index.js"`
+        exec: `cd build && cf push ${pivotal.appName} -c "node index.js"`
       }
     },
-    env: { aws, pivotal }
+    env: { pivotal }
   });
 
-  // mkdir for zip archive
-  grunt.registerTask("mkdir", function () {
-    grunt.file.mkdir('build/zip/temp');
-    grunt.file.mkdir('build/zip/uploads');
-    grunt.file.mkdir('build/zip/public');
+  grunt.registerTask("mkdir",  () => {
+    grunt.file.mkdir("build/public");
   });
 
-  // registerTask(taskName, taskList)
-  grunt.registerTask("build:aws", ["clean", "run:report", "env:aws", "run:buildFrontend", "mkdir", "copy:main", "compress"]);
   grunt.registerTask("copy:files", ["copy:main"]);
   grunt.registerTask("pivotal:build", ["clean", "env:pivotal", "run:buildFrontend", "mkdir", "copy:main"]);
   grunt.registerTask("pivotal:publish", ["run:pivotal"]);
--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ import compression from "compression";
 
 import serverSetup from "./setup";
 
+import path from "path";
+
 const logger = require("morgan");
 
 const _init = async () => {
@@ -25,11 +27,15 @@ const _init = async () => {
   app.use(logger("dev")); 
 
   app.use(passport.initialize());
-
+  
   // Initialize database, authentication, routes, etc ...
   await serverSetup(app, passport, { wipeDatabase: false, createDummyUser: true });
 
+  // Connects frontend build
+  app.use("/", express.static(path.join(__dirname, "public")));
+  app.get('*', (_req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
   app.listen(app.get("port"), () => console.log(`App listening on port ${app.get("port")}`));
+  return app;
 };
 
 _init();
