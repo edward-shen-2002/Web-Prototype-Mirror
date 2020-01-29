5c32e1562e2265b1242ad76024117ac5869aa166
Alfred Lemon <greymagez@hotmail.com>
2020-01-29 13:07:04 -0500

    Remove context menu state

--- frontend/src/store/actions/ui/excel/isContextMenuEnabled.js
+++
@@ -1,4 +0,0 @@
-import { ENABLE_CONTEXT_MENU, DISABLE_CONTEXT_MENU } from "@actionCreators";
-
-export const enableContextMenu = () => ({ type: ENABLE_CONTEXT_MENU });
-export const disableContextMenu = () => ({ type: DISABLE_CONTEXT_MENU });
--- frontend/src/store/reducers/ui/excel/isContextMenuEnabled.js
+++
@@ -1,9 +0,0 @@
-import { createReducer } from "@store/tools/setup";
-
-const SET_CONTEXT_MENU_ENALBED = () => true;
-
-const DISABLE_CONTEXT_MENU = () => false;
-
-const isContextMenuEnabledReducer = createReducer(false, { SET_CONTEXT_MENU_ENALBED, DISABLE_CONTEXT_MENU });
-
-export default isContextMenuEnabledReducer;
