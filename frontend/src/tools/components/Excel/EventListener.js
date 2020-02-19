949094de6f1670ea211c9488a212d5642e15436d
Alfred Lemon <greymagez@hotmail.com>
2020-02-19 08:28:06 -0500

    Add selection area with merged cells

--- frontend/src/tools/components/Excel/EventListener.js
+++ frontend/src/tools/components/Excel/EventListener.js
@@ -1820,10 +1820,135 @@ class EventListener extends PureComponent {
     this.updateActiveCellPosition(y1, x1, false);
   }
 
-  selectOver(x2, y2, ctrlKey) {
+  _getSelectionAreaColumn({
+    startX,
+    endX,
+
+    x1,
+    x2,
+    y1,
+    y2,
+
+    rowData,
+  }) {
+    for(let column = startX; column <= endX; column++) {
+      const cellData = rowData[column];
+
+      if(cellData && cellData.merged) {
+        const {
+          x1: mergedX1, 
+          x2: mergedX2, 
+          y1: mergedY1, 
+          y2: mergedY2 
+        } = cellData.merged;
+
+        const minMergedX = Math.min(mergedX1, mergedX2);
+        const maxMergedX = Math.max(mergedX1, mergedX2);
+        const minMergedY = Math.min(mergedY1, mergedY2);
+        const maxMergedY = Math.max(mergedY1, mergedY2);
+
+        if(minMergedX < x1) x1 = minMergedX;
+        if(minMergedY < y1) y1 = minMergedY;
+        if(maxMergedX > x2) x2 = maxMergedX;
+        if(maxMergedY > y2) y2 = maxMergedY;
+      }
+    }
+
+    return { x1, x2, y1, y2 };
+  }
+
+  _getSelectionAreaRow({
+    startY,
+    endY,
+
+    x1,
+    x2,
+    y1,
+    y2,
+
+    column,
+
+    sheetCellData
+  }) {
+    for(let row = startY; row <= endY; row++) {
+      const rowData = sheetCellData[row];
+
+      if(rowData && rowData[column] && rowData[column].merged) {
+        const { 
+          x1: mergedX1, 
+          x2: mergedX2, 
+          y1: mergedY1, 
+          y2: mergedY2 
+        } = rowData[column].merged;
+
+        const minMergedX = Math.min(mergedX1, mergedX2);
+        const maxMergedX = Math.max(mergedX1, mergedX2);
+        const minMergedY = Math.min(mergedY1, mergedY2);
+        const maxMergedY = Math.max(mergedY1, mergedY2);
+
+        if(minMergedX < x1) x1 = minMergedX;
+        if(minMergedY < y1) y1 = minMergedY;
+        if(maxMergedX > x2) x2 = maxMergedX;
+        if(maxMergedY > y2) y2 = maxMergedY;
+      }
+    }
+
+    return { x1, x2, y1, y2 };
+  }
+
+  _getNewArea({
+    currentArea,
+    sheetCellData
+  }) { 
+    const { x1, x2, y1, y2 } = currentArea;
+
+    let newArea = { ...currentArea };
+
+    const top = sheetCellData[y1];
+    const bottom = sheetCellData[y2];
+
+    if(top) {
+      newArea = this._getSelectionAreaColumn({ 
+        startX: newArea.x1, 
+        endX: newArea.x2,
+        rowData: top,
+        ...newArea
+      });
+    }
+
+    if(bottom) {
+      newArea = this._getSelectionAreaColumn({ 
+        startX: newArea.x1, 
+        endX: newArea.x2,
+        rowData: bottom,
+        ...newArea
+      });
+    }
+
+    // Do not need to check edges because top and bottom covers those already
+    // Between left
+    newArea = this._getSelectionAreaRow({
+      startY: newArea.y1 + 1,
+      endY: newArea.y2 - 1,
+      sheetCellData,
+      column: x1,
+      ...newArea
+    })
+
+    newArea = this._getSelectionAreaRow({
+      startY: newArea.y1 + 1,
+      endY: newArea.y2 - 1,
+      sheetCellData,
+      column: x2,
+      ...newArea
+    });
+
+    return newArea;
+  }
+
+  selectOver(newX2, newY2, ctrlKey) {
     const { 
       sheetCellData,
-      activeSelectionArea,
       isSelectionMode, 
       activeCellPosition, 
       stagnantSelectionAreas, 
@@ -1842,11 +1967,35 @@ class EventListener extends PureComponent {
 
     const { x, y } = activeCellPosition;
 
-    if(x === x2 && y === y2 && !ctrlKey) {
+    if(x === newX2 && y === newY2 && !ctrlKey) {
       this.resetActiveSelectionArea();
       handleResetActiveCellSelectionAreaIndex();
     } else {
-      handleUpdateActiveSelectionArea({ x1: x, y1: y, x2, y2 });
+      let minY = Math.min(y, newY2);
+      let minX = Math.min(x, newX2);
+      let maxY = Math.max(y, newY2);
+      let maxX = Math.max(x, newX2);
+      
+      let currentArea = {
+        y1: minY,
+        x1: minX,
+        y2: maxY,
+        x2: maxX
+      };
+
+      let newArea = this._getNewArea({ currentArea, sheetCellData });
+
+      while(
+        newArea.x1 !== currentArea.x1
+        || newArea.x2 !== currentArea.x2
+        || newArea.y1 !== currentArea.y1
+        || newArea.y2 !== currentArea.y2
+      ) {
+        currentArea = newArea;
+        newArea = this._getNewArea({ currentArea: newArea, sheetCellData });
+      }
+
+      handleUpdateActiveSelectionArea(newArea);
       if(stagnantSelectionAreasLength !== activeCellSelectionAreaIndex) handleUpdateActiveCellSelectionAreaIndex(stagnantSelectionAreasLength);
     }
   }
