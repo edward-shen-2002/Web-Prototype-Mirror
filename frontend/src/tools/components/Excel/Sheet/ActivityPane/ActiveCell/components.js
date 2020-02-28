e34320eb2f81082969bf5d43acc4ab28d897df37
Alfred Lemon <greymagez@hotmail.com>
2020-02-28 11:15:41 -0500

    Fix eslint issues

--- frontend/src/tools/components/Excel/Sheet/ActivityPane/ActiveCell/components.js
+++ frontend/src/tools/components/Excel/Sheet/ActivityPane/ActiveCell/components.js
@@ -4,6 +4,10 @@ import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Avatar from "@material-ui/core/Avatar";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import IconButton from "@material-ui/core/IconButton";
 
 export const DialogActions = ({
   handleAdd,
