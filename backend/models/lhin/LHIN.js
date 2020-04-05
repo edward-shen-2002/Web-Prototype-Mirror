0974479966b0fb52bbdbb59adb61198685f5fc47
Lemon <greymagez@hotmail.com>
2020-04-04 23:12:54 -0400

    [Breaking] Refactor flle hierarchy for new model

--- backend/models/lhin/LHIN.js
+++
@@ -1,7 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const LHINSchema = new Schema({
-  name: { type: String, required: true, unique: true }
-});
-
-export default model("LHIN", LHINSchema);
--- backend/models/sector/Sector.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-// Sector is one level above organization. A sector can contain a group of organizations, 
-// ?and organizations can only belong in one sector?
-const sectorSchema = new Schema({
-  name: { type: String, required: true, unique: true }
-}, { minimize: false });
-
-export default model("Sector", sectorSchema);
