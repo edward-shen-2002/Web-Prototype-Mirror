1bdbe1ec04cc7693a3d2ff3d2c07d40bd8ffaab9
Lemon <greymagez@hotmail.com>
2020-04-09 10:22:58 -0400

    Convert closure to class

--- backend-v2/loaders/database/database.ts
+++ backend-v2/loaders/database/database.ts
@@ -2,23 +2,7 @@ import * as mongoose from 'mongoose'
 
 import { DATABASE_KEY } from '../../config/database'
 
-import UserModel from '../../models/User'
-
-import OrganizationModel from '../../models/Organization'
-import OrganizationGroupModel from '../../models/OrganizationGroup'
-
-import ProgramModel from '../../models/Program'
-import SubmissionModel from '../../models/Submission'
-import TemplateModel from '../../models/Template'
-
-import CategoryGroupHierarchyModel from '../../models/CategoryGroupHierarchy'
-import CategoryGroupModel from '../../models/CategoryGroup'
-import ReportPeriodModel from '../../models/ReportPeriod'
-import YearModel from '../../models/Year'
-
-import MasterValueModel from '../../models/MasterValue'
-
-import { IOptions, WipeDatabase, CreateDummyData, ApplyOptions } from './interface'
+import { IOptions, IDatabase } from './interface'
 
 import IModels from '../../models/interface'
 
@@ -29,128 +13,108 @@ const defaultOptions: IOptions = {
   shouldCreateDummyData   : false
 }
 
-/**
- * Connects to and creates the configuration for the database
- */
-const initializeMongoose = async () => {
-  try {
-    console.log(logTag, 'Connecting...')
-    mongoose.set('autoCreate', true)
+export default class Database implements IDatabase {
+  public models: IModels
 
-    await mongoose.connect(
-      DATABASE_KEY, 
-      { 
-        useNewUrlParser     : true, 
-        useCreateIndex      : true, 
-        useFindAndModify    : false, 
-        useUnifiedTopology  : true 
-      }
-    )
+  public async initialize (customOptions: IOptions = defaultOptions) {
+    try {
+      console.log(logTag, 'Initializing...')
+      await this.initializeMongoose()
 
-    console.log(logTag, 'Connection successful')
-  } catch(error) {
-    throw `${logTag}Failed to connect to database\n${error}`
+      this.loadModels()
+
+      await this.applyOptions({ ...defaultOptions, ...customOptions })
+
+      console.log(logTag, 'Initialize successful')
+    } catch(error) {
+      throw `${logTag} Failed to initialize`
+    }
   }
-}
 
-/**
- * Populates the database with test data
- * 
- * @param models The data models of the database
- */
-const createDummyData: CreateDummyData = async (models) => {
-  try {
-    console.log(logTag, 'Creating dummy data...')
-
-
-    ///////////////////
-    // ! TODO
-    ///////////////////
-
-    console.log(logTag, 'Created dummy data')
-  } catch(error) {
-    throw `${logTag}Failed to create dummy data`
+  public async applyOptions({ shouldCreateDummyData, shouldWipeDatabase }) {
+    try {
+      console.log(logTag, 'Applying options...')
+  
+      if(shouldWipeDatabase) await this.wipeDatabase()
+      if(shouldCreateDummyData) await this.createDummyData(this.models)
+  
+      console.log(logTag, 'Applied options')
+    } catch(error) {
+      throw `${logTag}Failed to apply options`
+    }
   }
-}
 
-/**
- * Clears the content of all collections in the database
- */
-const wipeDatabase: WipeDatabase = async () => {
-  try {
-    console.warn(logTag, 'Wiping database...')
+  public loadModels() {
+    if(this.models) return
 
-    await mongoose.connection.dropDatabase()
-
-    console.warn(logTag, 'Wiped database')
-  } catch(error) {
-    throw `${logTag}Failed to wipe database`
-  }
-}
-
-const applyOptions: ApplyOptions = async (
-  {
-    shouldCreateDummyData,
-    shouldWipeDatabase
-  },
-  models
-) => {
-  try {
-    console.log(logTag, 'Applying options...')
-
-    if(shouldWipeDatabase) await wipeDatabase()
-    if(shouldCreateDummyData) await createDummyData(models)
-
-    console.log(logTag, 'Applied options')
-  } catch(error) {
-    throw `${logTag}Failed to apply options`
-  }
-}
-
-/**
- * Closure MongoDB setup.
- * 
- * @param customOptions Database options to trigger additional features
- */
-export default (
-  () => {
-    const models: IModels = {
-      UserModel,
+    this.models = {
+      UserModel: require('../../models/User'),
       
-      OrganizationModel,
-      OrganizationGroupModel,
+      OrganizationModel: require('../../models/Organization'),
+      OrganizationGroupModel: require('../../models/OrganizationGroup'),
 
-      ProgramModel,
-      SubmissionModel,
-      TemplateModel,
+      ProgramModel: require('../../models/Program'),
+      SubmissionModel: require('../../models/Submission'),
+      TemplateModel: require('../../models/Template'),
 
-      CategoryGroupHierarchyModel,
-      CategoryGroupModel,
-      ReportPeriodModel,
-      YearModel,
+      CategoryGroupHierarchyModel: require('../../models/CategoryGroupHierarchy'),
+      CategoryGroupModel: require('../../models/CategoryGroup'),
+      ReportPeriodModel: require('../../models/ReportPeriod'),
+      YearModel: require('../../models/Year'),
 
-      MasterValueModel
+      MasterValueModel: require('../../models/MasterValue')
     }
-
-    const initialize = async (customOptions: IOptions = defaultOptions) => {
-      try {
-        console.log(logTag, 'Initializing...')
-        await initializeMongoose()
-
-        await applyOptions(
-          { ...defaultOptions, ...customOptions },
-          models
-        )
-
-        console.log(logTag, 'Initialize successful')
-      } catch(error) {
-        throw `${logTag} Failed to initialize`
-      }
-    }
-
-    return () => ({
-      models,
-      initialize
-    })
   }
-)()
+
+  public async wipeDatabase() {
+    try {
+      console.warn(logTag, 'Wiping database...')
+
+      await mongoose.connection.dropDatabase()
+
+      console.warn(logTag, 'Wiped database')
+    } catch(error) {
+      throw `${logTag}Failed to wipe database`
+    }
+  }
+
+  /**
+   * Connects to and creates the configuration for the database
+   */
+  public async initializeMongoose() {
+    try {
+      console.log(logTag, 'Connecting...')
+      mongoose.set('autoCreate', true)
+
+      await mongoose.connect(
+        DATABASE_KEY, 
+        { 
+          useNewUrlParser     : true, 
+          useCreateIndex      : true, 
+          useFindAndModify    : false, 
+          useUnifiedTopology  : true 
+        }
+      )
+
+      console.log(logTag, 'Connection successful')
+    } catch(error) {
+      throw `${logTag}Failed to connect to database\n${error}`
+    }
+  }
+
+
+  public async createDummyData(models) {
+    try {
+      console.log(logTag, 'Creating dummy data...')
+
+      ///////////////////
+      // ! TODO
+      ///////////////////
+
+
+      console.log(logTag, 'Created dummy data')
+    } catch(error) {
+      throw `${logTag}Failed to create dummy data`
+    }
+  }
+}
--- backend-v2/loaders/database/interface.ts
+++ backend-v2/loaders/database/interface.ts
@@ -1,24 +1,28 @@
 import IModels from '../../models/interface'
 
-interface IDatabasesInstance {
+/**
+ * Methods to prepopulate databases with sample data for testing purposes
+ */
+
+/**
+ * Populates the database with test data
+ * 
+ * @param models The data models of the database
+ */
+export type CreateDummyData = (models: IModels) => Promise<void>
+type WipeDatabase = () => Promise<void>
+type ApplyOptions = (options: IOptions) => Promise<void>
+
+/**
+ * MongoDB setup.
+ */
+export interface IDatabase {
   wipeDatabase: WipeDatabase
   createDummyData: CreateDummyData
+  applyOptions: ApplyOptions
 }
 
 export interface IOptions {
-  shouldWipeDatabase?     : boolean
-  shouldCreateDummyData?  : boolean
+  shouldWipeDatabase     : boolean
+  shouldCreateDummyData  : boolean
 }
-
-/**
- * Function to prepopulate databases with sample data for testing purposes
- * 
- * @template IDatabases 
- */
-export type CreateDummyData = (models: IModels) => Promise<void>
-export type WipeDatabase = () => Promise<void>
-export type ApplyOptions = (options: IOptions, models: IModels) => Promise<void>
-
-export type DatabasesInstance = () => IDatabasesInstance
-
-export type DatabasesContainer = () => DatabasesInstance
