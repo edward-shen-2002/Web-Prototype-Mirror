6a15ac503308a5a42ad0075e41ad5ee73cbb18a7
Lemon <greymagez@hotmail.com>
2020-04-24 11:33:27 -0400

    Change await to promise

--- backend/src/loaders/database/database.ts
+++ backend/src/loaders/database/database.ts
@@ -20,77 +20,55 @@ export default class Database implements IDatabase {
   public repositories: IRepositories
 
   public async initialize(customOptions = defaultOptions) {
-    try {
-      console.log(logTag, 'Initializing...')
-      await this.initializeMongoose()
+    console.log(logTag, 'Initializing...')
 
-      // await this.applyOptions({ ...defaultOptions, ...customOptions })
-
-      console.log(logTag, 'Initialize successful')
-    } catch (error) {
-      throw `${logTag} Failed to initialize ${error}`
-    }
+    return this.initializeMongoose()
+      .then(() => console.log(logTag, 'Initialize successful'))
+      .catch((error) => {
+        throw `${logTag} Failed to initialize \n${error}`
+      })
   }
 
-  public async applyOptions({ shouldCreateDummyData, shouldWipeDatabase }) {
-    try {
-      console.log(logTag, 'Applying options...')
-
-      if (shouldWipeDatabase) await this.wipeDatabase()
-      if (shouldCreateDummyData) await this.createDummyData(this.repositories)
-
-      console.log(logTag, 'Applied options')
-    } catch (error) {
-      throw `${logTag}Failed to apply options`
-    }
-  }
+  public async applyOptions({ shouldCreateDummyData, shouldWipeDatabase }) {}
 
   public loadRepositories() {}
 
   public async wipeDatabase() {
-    try {
-      console.warn(logTag, 'Wiping database...')
-
-      await mongoose.connection.dropDatabase()
-
-      console.warn(logTag, 'Wiped database')
-    } catch (error) {
-      throw `${logTag}Failed to wipe database`
-    }
+    console.warn(logTag, 'Wiping database...')
+    return mongoose.connection
+      .dropDatabase()
+      .then(() => console.warn(logTag, 'Wiped database'))
+      .catch((error) => {
+        throw `${logTag}Failed to wipe database\n${error}`
+      })
   }
 
   /**
    * Connects to and creates the configuration for the database
    */
   public async initializeMongoose() {
-    try {
-      console.log(logTag, 'Connecting...')
-      // mongoose.set('autoCreate', true)
-
-      await mongoose.connect(DATABASE_KEY, {
+    console.log(logTag, 'Connecting...')
+    return mongoose
+      .connect(DATABASE_KEY, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true,
       })
-
-      console.log(logTag, 'Connection successful')
-    } catch (error) {
-      throw `${logTag}Failed to connect to database\n${error}`
-    }
+      .then(() => console.log(logTag, 'Connection successful'))
+      .catch((error) => {
+        throw `${logTag}Failed to connect to database\n${error}`
+      })
   }
 
   public async createDummyData(models) {
-    try {
-      console.log(logTag, 'Creating dummy data...')
+    console.log(logTag, 'Creating dummy data...')
 
-      // /////////////////
-      // ! TODO
-      // /////////////////
-
-      console.log(logTag, 'Created dummy data')
-    } catch (error) {
-      throw `${logTag}Failed to create dummy data`
-    }
+    // .then(() => {
+    console.log(logTag, 'Created dummy data')
+    // })
+    // .catch(() => {
+    //   throw `${logTag}Failed to create dummy data`
+    // })
   }
 }
