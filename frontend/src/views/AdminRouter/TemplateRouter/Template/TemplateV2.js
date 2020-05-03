3d965c6db851f5c8726866e05992f7b02c8e95df
Lemon <greymagez@hotmail.com>
2020-05-02 23:15:05 -0400

    Add template page

---
+++ frontend/src/views/AdminRouter/TemplateRouter/Template/TemplateV2.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+import { batch, useDispatch } from "react-redux";
+
+import { showAppNavigation, hideAppNavigation } from "@actions/ui/isAppNavigationOpen"; 
+
+import { 
+  setExcelData,
+  resetExcelData
+} from "@actions/ui/excel/commands";
+
+import { adminTemplateRoleAxios } from "@tools/rest";
+import Excel from "@tools/components/Excel";
+
+import { convertStateToReactState } from "@tools/excel";
+
+import { REST_ADMIN_TEMPLATES } from "@constants/rest";
+import { ROUTE_ADMIN_TEMPLATE_TEMPLATES } from "@constants/routes";
+
+import Loading from "@tools/components/Loading";
+
+import "./Template.scss";
+
+const Template = ({ match: { params: { _id } } }) => {
+  const dispatch = useDispatch()
+
+  const isCallInProgress = useSelector(
+    (
+      {
+        TemplateStore: {
+          isCallInProgress
+        }
+      }
+    ) => isCallInProgress
+  )
+
+  useEffect(
+    () => {
+      dispatch(getTemplateRequest(_id))
+    }, 
+    []
+  );
+
+  return (
+    isCallInProgress 
+      ? <Excel type="template" returnLink={ROUTE_ADMIN_TEMPLATE_TEMPLATES}/>
+      : <Loading/>
+  );
+};
+
+export default Template;
