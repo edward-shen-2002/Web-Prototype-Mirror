eda54a57314a2a7f8ed482b890c5edac7318eacd
Alfred Lemon <greymagez@hotmail.com>
2020-05-19 11:13:14 -0400

    Remove unused interfaces

--- backend/src/loaders/database/database.ts
+++ backend/src/loaders/database/database.ts
@@ -1,10 +1,8 @@
 import mongoose from 'mongoose'
 
-import { IOptions, IDatabase } from './interface'
+import { IDatabase } from './interface'
 
-import IRepositories from '../../repositories/interface'
 import { DATABASE_KEY } from '../../configs/database'
-import COAModel from '../../models/COA'
 
 const logTag = '[DB][MongoDB]: '
 
@@ -12,8 +10,6 @@ const logTag = '[DB][MongoDB]: '
 // TODO: DO NOT LOAD REPOSITORIES HERE - Singleton database connection
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 export default class Database implements IDatabase {
-  public repositories: IRepositories
-
   public connect() {
     console.log(logTag, 'Initializing...')
 
@@ -31,19 +27,13 @@ export default class Database implements IDatabase {
    */
   public initializeMongoose() {
     console.log(logTag, 'Connecting...')
+
     mongoose.connect(DATABASE_KEY, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true
-    }).then(
-      (db) => {
-        console.log('connected')
-        // console.log(db)
-        COAModel.find({}).then((COAs) => console.log('COAS', COAs))
-        console.log(db.modelNames())
-      }
-    )
+    })
 
     console.log(logTag, 'Connection successful')
   }
--- backend/src/repositories/interface.ts
+++ backend/src/repositories/interface.ts
@@ -1,25 +1,3 @@
-import IUserRepository from './User/interface'
-
-import IOrganizationRepository from './Organization/interface'
-import IOrganizationGroupRepository from './OrganizationGroup/interface'
-
-import IProgramRepository from './Program/interface'
-import ISubmissionRepository from './Submission/interface'
-import ITemplateRepository from './Template/interface'
-
-import IReportingPeriodRepository from './ReportingPeriod/interface'
-
-import IMasterValueRepository from './MasterValue/interface'
-
-import UserEntity from '../entities/User'
-import OrganizationEntity from '../entities/Organization'
-import OrganizationGroupEntity from '../entities/OrganizationGroup'
-import ProgramEntity from '../entities/Program'
-import SubmissionEntity from '../entities/Submission'
-import Template from '../entities/Template'
-import ReportingPeriod from '../entities/ReportingPeriod'
-import Year from '../entities/Year'
-import MasterValueEntity from '../entities/MasterValue'
 import { IId } from '../models/interface'
 
 // https://medium.com/@erickwendel/generic-repository-with-typescript-and-node-js-731c10a1b98e
@@ -39,17 +17,3 @@ interface IValidation<T> {
 }
 
 export interface IRepository<T> extends IWrite<T>, IRead<T>, IValidation<T> {}
-
-export default interface IRepositories {
-  UserRepository: IUserRepository<UserEntity>
-
-  OrganizationRepository: IOrganizationRepository<OrganizationEntity>
-  OrganizationGroupRepository: IOrganizationGroupRepository<OrganizationGroupEntity>
-
-  ProgramRepository: IProgramRepository<ProgramEntity>
-  SubmissionRepository: ISubmissionRepository<SubmissionEntity>
-  TemplateRepository: ITemplateRepository<Template>
-  ReportPeriodRepository: IReportingPeriodRepository<ReportingPeriod>
-
-  MasterValueRepository: IMasterValueRepository<MasterValueEntity>
-}
