79fe820345b66929e77f6cd008138517a030570a
Edward Shen <edward.shen@ontario.ca>
2020-06-18 23:50:31 -0400

    Merge with Bill's code

---
+++ gdct-app/frontend/src/store/thunks/Program.js
@@ -0,0 +1,26 @@
+import programController from '../../controllers/Program'
+import ProgramsStore from '../ProgramsStore/store'
+
+import {
+    getRequestFactory,
+    createRequestFactory,
+    deleteRequestFactory,
+    updateRequestFactory,
+} from './common/REST'
+
+export const getProgramsRequest = getRequestFactory(
+    ProgramsStore,
+    programController
+)
+export const createProgramsRequest = createRequestFactory(
+    ProgramsStore,
+    programController
+)
+export const deleteProgramsRequest = deleteRequestFactory(
+    ProgramsStore,
+    programController
+)
+export const updateProgramsRequest = updateRequestFactory(
+    ProgramsStore,
+    programController
+)
