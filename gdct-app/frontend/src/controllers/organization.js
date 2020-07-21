d50101283c390400e4e2c81627516e65cb524d27
George Chen <ggchen2k2@gmail.com>
2020-07-21 12:57:13 -0400

    Update controller path to new convention

--- gdct-app/frontend/src/controllers/organization.js
+++ gdct-app/frontend/src/controllers/organization.js
@@ -5,11 +5,11 @@ const orgController = (() => {
     baseURL: 'http://localhost:3000/organizations',
   });
   return {
-    fetchOrg: async _id => orgAxios.get(`/get/${_id}`).then(res => res.data.Org),
-    fetch: async () => orgAxios.get('/get').then(res => res.data.Orgs),
-    create: async Org => orgAxios.post('/create', { Org }).then(res => res.data.Org),
-    delete: async _id => orgAxios.delete(`/delete/${_id}`),
-    update: async Org => orgAxios.put(`/update/${Org._id}`, { Org }),
+    fetchOrg: async _id => orgAxios.get(`/fetchOrganization/${_id}`).then(res => res.data.Org),
+    fetch: async () => orgAxios.get('/fetchOrganization').then(res => res.data.Orgs),
+    create: async Org => orgAxios.post('/createOrganization', { Org }).then(res => res.data.Org),
+    delete: async _id => orgAxios.delete(`/deleteOrganization/${_id}`),
+    update: async Org => orgAxios.put(`/updateOrganization/${Org._id}`, { Org }),
     fetchByOrgGroupId: async _id =>
       orgAxios.get(`/searchOrganization/${_id}`).then(res => res.data.Org),
   };
